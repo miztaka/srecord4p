a78662c1bda551f96f179a87318eb22d97d4ce5a
miztaka@gmail.com <miztaka@gmail.com@933d4868-353e-43ea-eb2a-2f5868969bf8>
2010-11-06 23:59:32 +0000

    Initial Import

---
+++ trunk/src/lib/srecord/Generator.php
@@ -0,0 +1,299 @@
+<?php
+
+/**
+ * generate entity class
+ *
+ * @package teeple
+ */
+class Srecord_Generator {
+
+    /**
+     * @var Logger
+     */
+    protected $log;
+    
+    protected $forceUpdate = TRUE;
+    
+    /**
+     * constructor
+     */
+    public function __construct() {
+        //$this->log = LoggerManager::getLogger(get_class($this));
+    }
+
+    /**
+     * generate entity
+     * 
+     */
+    public function execute() {
+        
+        // connect to salesforce
+        $client = Srecord_Schema::getClient(); 
+        
+        // get object definition
+        $objectTypes = array();
+        $global = $client->describeGlobal();
+        foreach($global->sobjects as $def) {
+            $objectTypes[] = $def->name;
+        }
+        
+        $objectDefs = array();
+        $offset = 0;
+        $length = 100;
+        $max = count($objectTypes);
+        while($offset < $max) {
+            $part = $client->describeSObjects(array_slice($objectTypes, $offset, $length));
+            foreach($part as $one) { // TODO arrayの+でうまくいかなかった。。
+                $objectDefs[] = $one;
+            }
+            $offset += $length;
+        }
+        
+        // generate class file
+        foreach($objectDefs as $def) {
+            $this->generate($def);
+        }
+    }
+
+    private function generate($def) {
+        
+        $outputdir = dirname(dirname(__FILE__));
+
+        // create directory
+        $base_dir = $outputdir .'/'. 'sobjectdef';
+        if (! file_exists($base_dir) && ! mkdir($base_dir)) {
+            print "could not make directory for sobjectdef: {$base_dir}";
+            exit;
+        }
+        $sobject_dir = $outputdir .'/'. 'sobject';
+        if (! file_exists($sobject_dir) && ! mkdir($sobject_dir)) {
+            print "could not make directory for sobject: {$sobject_dir}";
+            exit;
+        }
+        
+        // metadata
+        $meta = base64_encode(serialize($def));
+        $soName = $def->name;
+        $soLabel = $def->label;
+        
+        $classname = "Sobject_{$soName}";
+        $baseclassname = "Sobjectdef_{$soName}";
+        
+        // childrelationship
+        $childRelationArrayStr = '';
+        $childRelationPropStr = '';
+        $childRelationships = array();
+        if (isset($def->childRelationships)) {
+            if (! is_array($def->childRelationships)) {
+                $def->childRelationships = array($def->childRelationships);
+            }
+            foreach ($def->childRelationships as $childdef) {
+                if (isset($childdef->relationshipName)) {
+                    $childRelationships[$childdef->relationshipName] = $childdef;
+                }
+            }
+            $childRelationArrayStr = $this->getChildRelationArrayStr($childRelationships);
+            $childRelationPropStr = $this->getChildRelationPropStr($childRelationships);
+        }
+        
+        // columns
+        $parentRelationArrayStr = "";
+        $parentRelationships = array();
+        $coldef = "";
+        $joindef = "";
+        if (! is_array($def->fields)) {
+            $def->fields = array($def->fields);
+        }
+        foreach($def->fields as $f) {
+            
+            $coldef .= "    public \$".$f->name."; //".$f->label."\n";
+            if ($f->type == 'reference' && isset($f->relationshipName)) {
+                $prop = $f->relationshipName;
+                $joindef .= <<<EOT
+    /**
+     * child-parent relationship to {$f->referenceTo}
+     * @var Sobject_{$f->referenceTo}
+     */
+    public \${$prop};
+
+
+EOT;
+                
+                $parentRelationships[$prop] = $f->referenceTo;
+            }
+        }
+        if (count($parentRelationships) > 0) {
+            $parentRelationArrayStr = $this->getParentRelationArrayStr($parentRelationships);
+        }
+            
+        // create base class 
+        $filepath = "{$base_dir}/{$soName}.php";
+        if (file_exists($filepath) && ! $this->forceUpdate) {
+            print "{$soName}: base class already exists. \n";
+            return;
+        }
+        if (!$handle = fopen($filepath, 'wb')) {
+            print "{$filepath}: could not open file. \n";
+            return;
+        }
+
+        $contents = <<<EOT
+/**
+ * Entity Base Class for {$soName} ({$soLabel})
+ *
+ * @package sobjectdef
+ */
+class {$baseclassname} extends Srecord_ActiveRecord
+{
+    /**
+     * name of sobject
+     * @var string
+     */
+    public static \$_SONAME = '{$soName}';
+    
+    /**
+     * columns
+     */
+{$coldef}
+
+{$joindef}
+{$childRelationPropStr}
+
+{$parentRelationArrayStr}
+
+{$childRelationArrayStr}
+
+    /**
+     * serialized metadata (don't delete this.)
+     */
+    public static \$__meta = '{$meta}';
+
+}
+EOT;
+
+        $contents = "<?php \n\n". $contents ."\n\n?>";
+        if (fwrite($handle, $contents) === FALSE) {
+            print "{$filepath}: failed to write to the file. \n";
+            return;
+        }
+
+        print "{$soName}: entity base class created . \n";
+        fclose($handle);
+
+        // create entity class
+        $filepath = "{$sobject_dir}/{$soName}.php";
+        if (file_exists($filepath)) {
+            print "{$soName}: entity class already exists. \n";
+            return;
+        }
+        if (!$handle = fopen($filepath, 'wb')) {
+            print "{$filepath}: could not open file. \n";
+            return;
+        }
+
+        $contents = <<<EOT
+/**
+ * Entity Class for {$soName} ({$soLabel})
+ * create domain logic here.
+ * @package sobject
+ */
+class {$classname} extends {$baseclassname}
+{
+    /**
+     * get instance of entity
+     * @return {$classname}
+     */
+    public static function get() {
+        return Teeple_Container::getInstance()->getEntity('{$classname}');
+    }
+    
+    /**
+     * find by key
+     * @param \$id
+     * @return {$classname}
+     */
+    public function find(\$id=null) {
+        return parent::find(\$id);
+    }
+    
+}
+EOT;
+
+        $contents = "<?php \n\n". $contents ."\n\n?>";
+        if (fwrite($handle, $contents) === FALSE) {
+            print "{$filepath}: failed to write to the file. \n";
+            return;
+        }
+
+        print "{$soName}: entity class created . \n";
+        fclose($handle);
+    }
+    
+    private function getChildRelationArrayStr($ar) {
+        
+        $keyval = "";
+        $tmp = array();
+        foreach ($ar as $key => $def) {
+            $name = $def->childSObject;
+            $tmp[] = "'{$key}' => '{$name}'";
+        }
+        $keyval = implode(",\n        ", $tmp);
+        
+        $result = <<<EOT
+    /**
+     * parent-child relationships
+     */
+    public static \$_childRelationships = array(
+        {$keyval}
+    );
+EOT;
+
+        return $result;
+    }
+    
+    private function getChildRelationPropStr($ar) {
+        
+        $template = <<<EOT
+    /**
+     * parent-child relationships to %s
+     * @var array 
+     */
+    public \$%s;
+
+
+EOT;
+
+        $result = "";
+        foreach ($ar as $key => $def) {
+            $objName = $def->childSObject;
+            $result .= sprintf($template, $objName, $key);
+        }
+        
+        return $result;
+    }
+    
+    private function getParentRelationArrayStr($ar) {
+        
+        $keyval = "";
+        $tmp = array();
+        foreach ($ar as $key => $def) {
+            $tmp[] = "'{$key}' => '{$def}'";
+        }
+        $keyval = implode(",\n        ", $tmp);
+        
+        $result = <<<EOT
+    /**
+     * child-parent relationships
+     */
+    public static \$_parentRelationships = array(
+        {$keyval}
+    );
+EOT;
+
+        return $result;
+    }
+    
+
+}
+
+?>
---
+++ trunk/src/test/ActiveRecordSelectTest.php
@@ -0,0 +1,107 @@
+<?php 
+
+require_once 'simpletestconfig.php';
+
+class ActiveRecordSelectTest extends UnitTestCase {
+    
+    public function testSelectClause() {
+        $account = new Sobject_Account();
+        $account->dryrun(TRUE);
+        
+        // simple
+        $query = $account->starts('Name', 'G')->select();
+        $expected = "SELECT Account.Id, Account.IsDeleted, Account.MasterRecordId, Account.Name, Account.Type, Account.ParentId, Account.BillingStreet, Account.BillingCity, Account.BillingState, Account.BillingPostalCode, Account.BillingCountry, Account.ShippingStreet, Account.ShippingCity, Account.ShippingState, Account.ShippingPostalCode, Account.ShippingCountry, Account.Phone, Account.Fax, Account.AccountNumber, Account.Website, Account.Sic, Account.Industry, Account.AnnualRevenue, Account.NumberOfEmployees, Account.Ownership, Account.TickerSymbol, Account.Description, Account.Rating, Account.Site, Account.OwnerId, Account.CreatedDate, Account.CreatedById, Account.LastModifiedDate, Account.LastModifiedById, Account.SystemModstamp, Account.LastActivityDate, Account.CustomerPriority__c, Account.SLA__c, Account.Active__c, Account.NumberofLocations__c, Account.UpsellOpportunity__c, Account.SLASerialNumber__c, Account.SLAExpirationDate__c, Account.email__c, Account.serviceName__c FROM Account WHERE (Name LIKE 'G%')";
+        $this->assertEqual(trim($query), $expected);
+        
+        // specify columns
+        $query = $account->ends('Name', 'G')->select('Id, Name');
+        $expected = "SELECT Account.Id, Account.Name FROM Account WHERE (Name LIKE '%G')";
+        $this->assertEqual(trim($query), $expected);
+        
+        // parent relationship
+        $query = $account
+            ->join('Owner')
+            ->select('Name');
+        $expected = "SELECT Account.Name, Account.Owner.Id, Account.Owner.Username, Account.Owner.LastName, Account.Owner.FirstName, Account.Owner.Name, Account.Owner.CompanyName, Account.Owner.Division, Account.Owner.Department, Account.Owner.Title, Account.Owner.Street, Account.Owner.City, Account.Owner.State, Account.Owner.PostalCode, Account.Owner.Country, Account.Owner.Email, Account.Owner.Phone, Account.Owner.Fax, Account.Owner.MobilePhone, Account.Owner.Alias, Account.Owner.CommunityNickname, Account.Owner.IsActive, Account.Owner.TimeZoneSidKey, Account.Owner.UserRoleId, Account.Owner.LocaleSidKey, Account.Owner.ReceivesInfoEmails, Account.Owner.ReceivesAdminInfoEmails, Account.Owner.EmailEncodingKey, Account.Owner.ProfileId, Account.Owner.UserType, Account.Owner.LanguageLocaleKey, Account.Owner.EmployeeNumber, Account.Owner.DelegatedApproverId, Account.Owner.ManagerId, Account.Owner.LastLoginDate, Account.Owner.LastPasswordChangeDate, Account.Owner.CreatedDate, Account.Owner.CreatedById, Account.Owner.LastModifiedDate, Account.Owner.LastModifiedById, Account.Owner.SystemModstamp, Account.Owner.OfflineTrialExpirationDate, Account.Owner.OfflinePdaTrialExpirationDate, Account.Owner.UserPermissionsMarketingUser, Account.Owner.UserPermissionsOfflineUser, Account.Owner.UserPermissionsCallCenterAutoLogin, Account.Owner.UserPermissionsMobileUser, Account.Owner.UserPermissionsSupportUser, Account.Owner.ForecastEnabled, Account.Owner.UserPreferencesActivityRemindersPopup, Account.Owner.UserPreferencesEventRemindersCheckboxDefault, Account.Owner.UserPreferencesTaskRemindersCheckboxDefault, Account.Owner.UserPreferencesReminderSoundOff, Account.Owner.UserPreferencesApexPagesDeveloperMode, Account.Owner.ContactId, Account.Owner.AccountId, Account.Owner.CallCenterId, Account.Owner.Extension, Account.Owner.FederationIdentifier FROM Account";
+        $this->assertEqual(trim($query), $expected);
+        
+        // specify columns of parent
+        $query = $account
+            ->join('Owner', 'Id, Username')
+            ->select('Name');
+        $expected = "SELECT Account.Name, Account.Owner.Id, Account.Owner.Username FROM Account";
+        $this->assertEqual(trim($query), $expected);
+        
+        // parent criteria
+        $query = $account
+            ->join('Owner', 'Id, Username')
+            ->contains('Name', 'G')
+            ->eq('Owner.Username', 'sato')
+            ->select('Name');
+        $expected = "SELECT Account.Name, Account.Owner.Id, Account.Owner.Username FROM Account WHERE (Name LIKE '%G%') AND (Owner.Username = 'sato')";
+        $this->assertEqual(trim($query), $expected);
+        
+        // child relationship
+        $query = $account
+            ->child('Notes')
+            ->select('Name');
+        $expected = "SELECT Account.Name, (SELECT Id, IsDeleted, ParentId, Title, IsPrivate, Body, OwnerId, CreatedDate, CreatedById, LastModifiedDate, LastModifiedById, SystemModstamp FROM Notes) FROM Account";
+        $this->assertEqual(trim($query), $expected);
+        
+        // specify columns of child
+        $query = $account
+            ->child('Notes', 'Id, Title')
+            ->select('Name');
+        $expected = "SELECT Account.Name, (SELECT Id, Title FROM Notes) FROM Account";
+        $this->assertEqual(trim($query), $expected);
+        
+        // conbination of parent, child
+        $query = $account
+            ->join('Owner', 'Id, Username')
+            ->child('Notes', 'Id, Title')
+            ->select('Name');
+        $expected = "SELECT Account.Name, Account.Owner.Id, Account.Owner.Username, (SELECT Id, Title FROM Notes) FROM Account";
+        $this->assertEqual(trim($query), $expected);
+        //print "#{$query}#";
+        
+    }
+    
+    public function testResultSet() {
+        
+        try {
+        $account = new Sobject_Account();
+        $result = $account
+            ->join('Owner','Id, Username')
+            ->join('CreatedBy','Username')
+            ->child('Contacts', 'Id, Name')
+            ->child('Cases', 'Id, Reason')
+            ->like('Name', 'G%')
+            ->select('Id, Name');
+        print("<pre>");
+        var_dump($result);
+        print("</pre>");
+        //SELECT Account.Id, Account.Name, Account.Owner.Id, Account.Owner.Username, Account.CreatedBy.Username, (SELECT Id, name FROM Contacts), (SELECT Id, reason FROM Cases) FROM Account WHERE Name LIKE 'G%'";
+        
+        $this->assertEqual(count($result), 3);
+        $this->assertEqual($result[0]->Id, '0018000000UoDxpAAF');
+        $this->assertEqual($result[0]->Name, 'GenePoint');
+        $this->assertEqual($result[0]->Owner->Username, 'miztaka@honestyworks.jp');
+        $this->assertEqual($result[0]->Owner->Id, '00580000002fHOEAA2');
+        $this->assertEqual($result[0]->CreatedBy->Username, 'miztaka@honestyworks.jp');
+        $this->assertEqual(count($result[0]->Contacts), 1);
+        $this->assertEqual($result[0]->Contacts[0]->Name, 'Frank Edna');
+        $this->assertEqual($result[0]->Contacts[0]->Id, '0038000000gVvdvAAC');
+        $this->assertEqual(count($result[0]->Cases), 2);
+        $this->assertEqual($result[0]->Cases[0]->Reason, 'Feedback');
+        $this->assertEqual($result[0]->Cases[0]->Id, '500800000075hBuAAI');
+        $this->assertEqual($result[0]->Cases[1]->Reason, 'Feedback');
+        $this->assertEqual($result[0]->Cases[1]->Id, '500800000075hC4AAI');
+        } catch (Exception $e) {
+            $this->fail($e->getTraceAsString());
+        }
+        
+    }
+    
+}
+
+?>
