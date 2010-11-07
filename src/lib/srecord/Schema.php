de9b8fb32851ef801a1bc0bc33b67d82b1caee38
miztaka <miztaka@gmail.com>
2010-11-07 09:27:38 +0000

    nested parent relationship

--- src/lib/srecord/Schema.php
+++ src/lib/srecord/Schema.php
@@ -9,17 +9,34 @@ class Srecord_Schema
     public static $securityToken = '';
     public static $wsdlPartner = '';
     
+    const SESSION_KEY = '__Srecord_Schema';
+    
     /**
      * connect to salesforce, get sforceClient
      * @return SforcePartnerClient
-     * TODO キャッシュ to Session
      */
     public static function getClient() {
         
         if (self::$_sForceClient == NULL) {
+            
             $sforceClient = new SforcePartnerClient(); // TODO タイプ指定できるように
             $soapClient = $sforceClient->createConnection(self::$wsdlPartner);
-            $sforceLogin = $sforceClient->login(self::$username, self::$password.self::$securityToken);
+            if (PHP_SAPI != 'cli') {
+                @session_start();
+                if (isset($_SESSION[self::SESSION_KEY])) {
+                    $conf = $_SESSION[self::SESSION_KEY];
+                    $sforceClient->setEndpoint($conf->location);
+                    $sforceClient->setSessionHeader($conf->sessionId);
+                } else {
+                    $sforceLogin = $sforceClient->login(self::$username, self::$password.self::$securityToken);
+                    $conf = new stdClass();
+                    $conf->location = $sforceClient->getLocation();
+                    $conf->sessionId = $sforceClient->getSessionId();
+                    $_SESSION[self::SESSION_KEY] = $conf;
+                }
+            } else {
+                $sforceLogin = $sforceClient->login(self::$username, self::$password.self::$securityToken);
+            }
             self::$_sForceClient = $sforceClient;
         }
         return self::$_sForceClient; 
